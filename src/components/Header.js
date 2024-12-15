bae3c8719b0be6a7513bdb39f2a06edf9752fc6a
Vavinan <e0959114@u.nus.edu>
2024-12-15 13:08:34 +0800

    Add LOGO: Add NFE logo to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './engineering.png';
+import logo from '../images/LOGO.png';
 
 const Header = () => {
   return (
@@ -8,27 +8,45 @@ const Header = () => {
       <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            <img src={logo} alt="NEW FACT ENGINEERING" height="40" />
+            <img src={logo} alt="NEW FACT ENGINEERING" className="logo" />
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item ms-4">
-                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
               </li>
               <li className="nav-item ms-4">
-                <Link className="nav-link" to="/services">Services</Link>
+                <Link className="nav-link" to="/services">
+                  Services
+                </Link>
               </li>
               <li className="nav-item ms-4">
-                <Link className="nav-link" to="/projects">Projects</Link>
+                <Link className="nav-link" to="/projects">
+                  Projects
+                </Link>
               </li>
               <li className="nav-item ms-4">
-                <Link className="nav-link" to="/social-service">Social Service</Link>
+                <Link className="nav-link" to="/social-service">
+                  Social Service
+                </Link>
               </li>
               <li className="nav-item ms-4">
-                <Link className="nav-link" to="/contact">Contact</Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
               </li>
             </ul>
           </div>
